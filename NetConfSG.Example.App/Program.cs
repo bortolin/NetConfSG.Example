a88b3c5df27828c2ebe2eed57fb66be4d88fe5d6
Marco Bortolin <bortolin.marco@gmail.com>
2024-03-18 18:35:10 +0100

    Refactor

--- NetConfSG.Example.App/Program.cs
+++ NetConfSG.Example.App/Program.cs
@@ -7,8 +7,8 @@ Console.WriteLine("");
 
 //Get all types end with Command name in the current assembly using reflection
 //This canno't be use with AOT compilation
-//Assembly.GetExecutingAssembly().GetTypes()
-//        .Where(t=>t.Name.EndsWith("Command")).ToList()
-//        .ForEach(t=>Console.WriteLine(t.Name));
+Assembly.GetExecutingAssembly().GetTypes()
+        .Where(t => t.Name.EndsWith("Command")).ToList()
+        .ForEach(t => Console.WriteLine(t.Name));
 
-CommandList.Commands.ToList().ForEach(c => Console.WriteLine(c));
+//CommandList.Commands.ToList().ForEach(c => Console.WriteLine(c));
